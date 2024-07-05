1282bdb8490027f9f6f1f8d1d15ba08a0e7aa8da
Theophilus Samuel <36473098+ConnectedDot@users.noreply.github.com>
2024-07-05 13:46:58 +0100

    Manipulations

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,20 @@
+function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  if (typeof startString !== 'string') {
+    return '';
+  }
+
+  if (startString === undefined) {
+    return '';
+  }
+
+  const setFiltered = Array.from(set).filter((element) => element.startsWith(startString))
+    .map((element) => element.slice(startString.length)).join('-');
+
+  return setFiltered;
+}
+
+module.exports = cleanSet;
